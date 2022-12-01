46d731541e6afff56ad125db986edeb8ec37ac63
John <john@Zipcoders-MacBook-Pro-46.local>
2022-12-01 15:50:46 -0500

    finished

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,43 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    String expectedName;
+    Long expectedId;
+    Double expectedPrice;
+
     public Item() {
+        expectedName = "";
+        expectedId = 0L;
+        expectedPrice = 0.01;
     }
 
     public Item(Long id, String name, Double price) {
+        this.expectedId = id;
+        this.expectedName = name;
+        this.expectedPrice = price;
     }
 
     public String getName() {
-        return null;
+        return expectedName;
     }
 
     public void setName(String name) {
+        this.expectedName = name;
     }
 
     public Long getId() {
-        return null;
+        return expectedId;
     }
 
     public void setId(Long id) {
+        this.expectedId = id;
     }
 
     public Double getPrice() {
-        return null;
+        return expectedPrice;
     }
 
     public void setPrice(Double price) {
+        this.expectedPrice = price;
     }
 }
